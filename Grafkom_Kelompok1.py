d46e4aab27d35eb1d1227af353d39adc8a423b43
rahmatlatif10 <77723387+rahmatlatif10@users.noreply.github.com>
2021-12-07 14:09:39 +0800

    Memasukkan Syarat Jika  X1=X2

--- Grafkom_Kelompok1.py
+++ Grafkom_Kelompok1.py
@@ -8,7 +8,15 @@ x2 = int(input('Masukan nilai x2  : '))
 y2 = int(input('Masukan nilai y2  : '))
 print('------------------------------------------------')
 
-
-# titik awal x dan y
 x = x1
 y = y1
+
+i = 1
+
+if x1 == x2:
+    titikA = []
+    titikB = []
+    for i in range (1,y2,1):
+        grafik.plot(titikA,titikB)
+        grafik.show()
+
