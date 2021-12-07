3613e3f034654ddb3cdae2fa06df98a9b12730cb
MuhammadZahir77 <95671914+MuhammadZahir77@users.noreply.github.com>
2021-12-07 13:34:10 +0800

    Membuat Inputan Titik Koordinat

---
+++ Grafkom_Kelompok1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as grafik
+
+
+x1 = int(input('Masukan nilai x1  : '))
+y1 = int(input('Masukan nilai y1  : '))
+x2 = int(input('Masukan nilai x2  : '))
+y2 = int(input('Masukan nilai y2  : '))
+print('------------------------------------------------')
+
+
+# titik awal x dan y
+x = x1
+y = y1
